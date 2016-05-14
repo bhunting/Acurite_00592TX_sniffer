8ab93585f251bb66578f4eb35722f7ff73bfcefc
Brad Hunting, Ph.D <bradhunting@gmail.com>
2016-05-14 13:08:01 -0600

    changed temperatue calculation, cleaned up code

--- acurite_00592TX_homearray/acurite_00592TX_homearray.ino
+++ acurite_00592TX_homearray/acurite_00592TX_homearray.ino
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Arduino code to sniff the Acurite 00592TX wireless temperatureerature
+ * Arduino code to sniff the Acurite 00592TX wireless temperature
  * probe output data stream.
  *
  * Ideas on decoding protocol and prototype code from
@@ -12,17 +12,17 @@
  * Code based on Ray Wang's humidity_display.ino source.
  * Heavily modified by Brad Hunting.
  *
- * The 00592TX wireless temperatureerature probe contains a 433 MHz
- * wireless transmitter. The temperatureerature from the probe is
+ * The 00592TX wireless temperature probe contains a 433 MHz
+ * wireless transmitter. The temperature from the probe is
  * sent approximately every 16 seconds.
  *
  * The 00592TX typically only sends one SYNC pulse + DATA stream
- * per temperatureerature reading. Infrequently two sync/data streams
+ * per temperature reading. Infrequently two sync/data streams
  * are sent during the same transmit window but that seems to 
  * be the exception.
  *
  * Ray Wang's code is for a different model of probe, one that 
- * transmits both temperatureerature and humidity. Ray' code relies on 
+ * transmits both temperature and humidity. Ray' code relies on 
  * two sync streams with a preceeding delay. 
  * 
  * The 00592TX usually starts the data sync bits right after
@@ -81,7 +81,7 @@
  *   When the remote probe batteries are fresh, voltage above 2.5V, the third byte is 0x44.
  *   When the remote probe batteries get low, below 2.4V, the third byte changes to 0x84.
  * The fourth byte continues to stay at 0x90 for all conditions.
- * The next two bytes are the temperatureerature. The temperatureerature is encoded as the
+ * The next two bytes are the temperature. The temperature is encoded as the
  *   lower 7 bits of both bytes with the most significant bit being an
  *   even parity bit.  The MSB will be set if required to insure an even
  *   number of bits are set to 1 in the byte. If the least significant
@@ -277,15 +277,13 @@ typedef struct sensortemperatureData
     uint8_t     id;             // sensor id (1 - N_sensors)
     uint8_t     status;         // 0x80 = BATTERY LOW
                                 // 0x40 = CRC ERROR
-    uint16_t    temperature;    // temperatureerature value in C, no offset
+    uint16_t    temperature;    // temperature value in C, no offset
     uint32_t    timestamp;      // number of seconds since startup
 } sensortemperatureData;
 
 static const uint8_t _numSensors = 6; // I happen to have 6 sensor probes
 
 static sensortemperatureData sensordata[_numSensors];
-
-static uint16_t temperature;
 
 static uint16_t idArray[_numSensors] = 
 { 0x0C34, 0x1E09, 0x26ED, 0x36E7, 0x0604, 0x386C };
@@ -352,9 +350,8 @@ void loop()
       // disable interrupt to avoid new data corrupting the buffer
       detachInterrupt(1);
 
-      // extract temperatureerature value
+      // convert bits to bytes
       unsigned int startIndex, stopIndex, ringIndex;
-      unsigned long temperatureerature = 0;
       uint8_t dataBytes[DATABYTESCNT];
       // clear the data bytes array
       for( int i = 0; i < DATABYTESCNT; i++ )
@@ -435,36 +432,15 @@ void loop()
       }
 
       // extract temperature value
+      uint16_t temperature = 0;
       sensordata[id].temperature = 0;
-      temperature = 0;
-      // most significant 4 bits
-      startIndex = (dataIndex + (4*8+4)*2) % RING_BUFFER_SIZE;
-      stopIndex  = (dataIndex + (4*8+8)*2) % RING_BUFFER_SIZE;
-      for( int i = startIndex; i != stopIndex; i = (i+2)%RING_BUFFER_SIZE )
-      {
-         int bit = convertTimingToBit(pulseDurations[i], pulseDurations[(i+1)%RING_BUFFER_SIZE]);
-         temperature = (temperature<<1) + bit;
-         if( bit < 0 )
-         {  
-            Serial.println("Bit Timing : Decoding error.");
-            return;      // exit due to error
-         }
-      }
-      // least significant 7 bits
-      startIndex = (dataIndex + (5*8+1)*2) % RING_BUFFER_SIZE;
-      stopIndex  = (dataIndex + (5*8+8)*2) % RING_BUFFER_SIZE;
-      for( int i=startIndex; i!=stopIndex; i=(i+2)%RING_BUFFER_SIZE )
-      {
-         int bit = convertTimingToBit( pulseDurations[i%RING_BUFFER_SIZE], 
-                                       pulseDurations[(i+1)%RING_BUFFER_SIZE] );
-         temperature = ( temperature << 1 ) + bit; // shift and insert next bit
-         if( bit < 0 )
-         {  
-            Serial.println("Bit Timing : Decoding error.");
-            return;      // exit due to error
-         }
-      }    
-
+      
+      // data bytes have already been decoded
+      // 7 bits are significant, 8th bit is even parity bit
+      // only shift by 7 bits because low byte is 7 bits of data
+      temperature = ((acurite_data->temperature_high) & 0x7F) << 7;
+      temperature += (acurite_data->temperature_low) & 0x7F;
+      // temperature is offset by 1024 (= 0x400 = b0100 0000 0000)
       sensordata[id].temperature = (uint16_t)((temperature-1024)+0.5);
       sensordata[id].timestamp = millis() / 1000;  // convert milli-seconds into seconds
       
